07b30d5ea3b9bc169edc4de8a598392573bc2b6e
pereyrafacundo706-dev <pereyrafacundo706@gmail.com>
2026-06-24 21:59:05 -0300

    Add files via upload

--- BobConstruye/src/clases/Gestor.java
+++ BobConstruye/src/clases/Gestor.java
@@ -8,9 +8,23 @@ public class Gestor {
 		
 		Casa casa1 = new Casa(null, 0, null, 0, 0, 0, null, null, 0);
 		Apartamento apart1 = new Apartamento (null, 0, null, 0, 0, 0, null, null, null);
+		int opcion = 0;
 		
-		System.out.println("\n=Bienvenido al programa=\n");
+		do {
+		System.out.println("\n=Bob construye=\n");
+		System.out.println("--Seleccione una opcion: --\n");
+		System.out.println("[1. Alta de casa       ]");
+		System.out.println("[2. Alta de apartamento]");
+		System.out.println("[3. Listar todo        ]");
+		System.out.println("[4. Salir              ]");
+		opcion = lector.nextInt();
+		
+		switch (opcion) {
+		
+		case 1:
+			
 		System.out.println("=REGISTRO CASA=");
+		lector.nextLine();
 		System.out.println("Ingrese nombre: ");
 		String nombreCasa = lector.nextLine();
 		System.out.println("ingrese id: ");
@@ -32,16 +46,11 @@ public class Gestor {
 		System.out.println("Ingrese numero de pisos: ");
 		int numPisos = lector.nextInt();
 		
-		casa1.setNombre(nombreCasa);
-		casa1.setId(idCasa);
-		casa1.setDireccion(dirCasa);
-		casa1.setCodPostal(codPostalCasa);
-		casa1.setAnioConstruccion(anioConstruccion);
-		casa1.setSuperficieTotal(superTotal);
-		casa1.setPropietario(propietario);
-		casa1.setGaraje(garaje);
-		casa1.setNumPisos(numPisos);
+		altaCasa(casa1, nombreCasa, idCasa, dirCasa, codPostalCasa, anioConstruccion, superTotal, propietario, garaje, numPisos);
+		break;
 		
+		case 2:
+			
 		System.out.println("=REGISTRO APARTAMENTO=");
 		lector.nextLine();
 		System.out.println("Ingrese nombre: ");
@@ -65,43 +74,81 @@ public class Gestor {
 		System.out.println("Tiene balcones: ");
 		String balcon = lector.next();
 		
-		apart1.setNombre(nombreApartamento);
-		apart1.setId(idApartamento);
-		apart1.setDireccion(dirApartamento);
-		apart1.setCodPostal(codPostalApartamento);
-		apart1.setAnioConstruccion(anioConstruccionApartamento);
-		apart1.setSuperficieTotal(superTotalApartamento);
-		apart1.setPropietario(propietarioApartamento);
-		apart1.setTorre(torre);
-		apart1.setBalcon(balcon);
+		altaApartamento(apart1, nombreApartamento, idApartamento, dirApartamento, codPostalApartamento, anioConstruccionApartamento, superTotalApartamento, propietarioApartamento, torre, balcon);
+		break;
+		
+		case 3:
+			
+		listaCasa (casa1);
+		listaApartamento (apart1);
+		break;
+		
+		case 4:
+			
+		System.out.println("Saliendo...");	
+		
+		}
+		
+		}while (opcion <=3);	
+
+	}
+	
+	public static void altaCasa (Casa casaA, String nombreCasa, int idCasa, String dirCasa, int codPostalCasa, int anioConstruccion, double superTotal, String propietario, String garaje, int numPisos) {
+		
+		casaA.setNombre(nombreCasa);
+		casaA.setId(idCasa);
+		casaA.setDireccion(dirCasa);
+		casaA.setCodPostal(codPostalCasa);
+		casaA.setAnioConstruccion(anioConstruccion);
+		casaA.setSuperficieTotal(superTotal);
+		casaA.setPropietario(propietario);
+		casaA.setGaraje(garaje);
+		casaA.setNumPisos(numPisos);
+	}
+	
+	public static void altaApartamento (Apartamento apartamentoB, String nombreApartamento, int idApartamento, String dirApartamento, int codPostalApartamento, int anioConstruccionApartamento, double superTotalApartamento, String propietarioApartamento, String torre, String balcon) {
+		
+		apartamentoB.setNombre(nombreApartamento);
+		apartamentoB.setId(idApartamento);
+		apartamentoB.setDireccion(dirApartamento);
+		apartamentoB.setCodPostal(codPostalApartamento);
+		apartamentoB.setAnioConstruccion(anioConstruccionApartamento);
+		apartamentoB.setSuperficieTotal(superTotalApartamento);
+		apartamentoB.setPropietario(propietarioApartamento);
+		apartamentoB.setTorre(torre);
+		apartamentoB.setBalcon(balcon);
+	
+	}
+	
+	public static void listaCasa (Casa casaA) {
 		
 		System.out.println("\n=CASA=\n");
-		System.out.println("-Nombre: "+casa1.getNombre());
-		System.out.println("-Id: "+casa1.getId());
-		System.out.println("-Direccion: "+casa1.getDireccion());	
-		System.out.println("-Codigo postal: "+casa1.getCodPostal());
-		System.out.println("-Año de finalizacion de la construccion: "+casa1.getAnioConstruccion());	
-		System.out.println("-Superficie total en metros cuadrados: "+casa1.getSuperficieTotal()+" m²");
-		System.out.println("-Nombre del propietario: "+casa1.getPropietario());	
-		System.out.println("-Disponibilidad de garaje: "+casa1.getGaraje());
-		System.out.println("-Numero de pisos: "+casa1.getNumPisos());
+		System.out.println("-Nombre: "+casaA.getNombre());
+		System.out.println("-Id: "+casaA.getId());
+		System.out.println("-Direccion: "+casaA.getDireccion());	
+		System.out.println("-Codigo postal: "+casaA.getCodPostal());
+		System.out.println("-Año de finalizacion de la construccion: "+casaA.getAnioConstruccion());	
+		System.out.println("-Superficie total en metros cuadrados: "+casaA.getSuperficieTotal()+" m²");
+		System.out.println("-Nombre del propietario: "+casaA.getPropietario());	
+		System.out.println("-Disponibilidad de garaje: "+casaA.getGaraje());
+		System.out.println("-Numero de pisos: "+casaA.getNumPisos());
+		
+	}
+	
+	public static void listaApartamento (Apartamento apartA) {
 		
 		System.out.println("\n=APARTAMENTO=\n");
-		System.out.println("-Nombre: "+apart1.getNombre());
-		System.out.println("-Id: "+apart1.getId());
-		System.out.println("-Direccion: "+apart1.getDireccion());	
-		System.out.println("-Codigo postal: "+apart1.getCodPostal());
-		System.out.println("-Año de finalizacion de la construccion: "+apart1.getAnioConstruccion());	
-		System.out.println("-Superficie total en metros cuadrados: "+apart1.getSuperficieTotal()+" m²");
-		System.out.println("-Nombre del propietario: "+apart1.getPropietario());	
-		System.out.println("-Bloques de apartamento: "+apart1.getTorre());
-		System.out.println("-Disponibilidad de balcones: "+apart1.getBalcon());
+		System.out.println("-Nombre: "+apartA.getNombre());
+		System.out.println("-Id: "+apartA.getId());
+		System.out.println("-Direccion: "+apartA.getDireccion());	
+		System.out.println("-Codigo postal: "+apartA.getCodPostal());
+		System.out.println("-Año de finalizacion de la construccion: "+apartA.getAnioConstruccion());	
+		System.out.println("-Superficie total en metros cuadrados: "+apartA.getSuperficieTotal()+" m²");
+		System.out.println("-Nombre del propietario: "+apartA.getPropietario());	
+		System.out.println("-Bloques de apartamento: "+apartA.getTorre());
+		System.out.println("-Disponibilidad de balcones: "+apartA.getBalcon());
 		
 		
-		
-		
-		
-
 	}
 
 }
